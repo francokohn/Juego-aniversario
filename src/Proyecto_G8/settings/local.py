f070e9e6b2da36045eef8201bca5c8cf2ea6788a
francokohn <francokohn@gmail.com>
2021-08-21 10:24:44 -0300

    Creacion app Proyecto final

---
+++ src/Proyecto_G8/settings/local.py
@@ -0,0 +1,14 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prestamos',
+        'USER': 'postgres',
+        'PASSWORD': '0207',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
+    }
+}
+

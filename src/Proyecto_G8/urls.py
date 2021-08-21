92917634b72f59d5cace627df3eaa6e877264449
francokohn <francokohn@gmail.com>
2021-08-21 14:11:30 -0300

    Agregue carpetas para la vista usuarios

--- src/Proyecto_G8/urls.py
+++ src/Proyecto_G8/urls.py
@@ -1,10 +1,9 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from .views import inicio
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('inicio/',inicio),
-
+    path('inicio/', inicio),
+    path('Usuarios/', include('apps.ProyectoFinal.urls'))
 ]
--- src/apps/ProyectoFinal/views.py
+++ src/apps/ProyectoFinal/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def listar_usuarios (request):
+	template_name = "usuarios/listar.html"
+
+	ctx = {
+	}	
+
+	return render(request,template_name,ctx)

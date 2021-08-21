ff1e5e8a9f707786daf6b662d6e7d357cf338a36
francokohn <francokohn@gmail.com>
2021-08-21 14:55:18 -0300

    Agregado campos en la creacion de usuarios que pase por alto

--- src/apps/ProyectoFinal/admin.py
+++ src/apps/ProyectoFinal/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import Usuario
 
 class UsuariosAdmin(admin.ModelAdmin):
-	list_display = ['id','nombre','nickname','localidad','ult_acceso']
+	list_display = ['id','nombre','nickname','localidad','ult_acceso','correo','es_admin']
 
 admin.site.register(Usuario, UsuariosAdmin)
--- src/apps/ProyectoFinal/models.py
+++ src/apps/ProyectoFinal/models.py
@@ -3,11 +3,10 @@ from django.db import models
 class Usuario(models.Model):
 	nombre = models.CharField(max_length=255)
 	nickname = models.CharField(max_length=50)
-	correo = models.EmailField
-	es_admin = models.BooleanField
+	correo = models.EmailField()
+	es_admin = models.BooleanField()
 	localidad = models.CharField(max_length=255)
-	ult_acceso = models.CharField(max_length=255)
-	puntos = models.IntegerField
+	ult_acceso = models.CharField(max_length=200, null=True, blank=True)
 
 	class Meta:
 		db_table = 'usuarios'

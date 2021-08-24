6e8356b0730a90a7ef09c36768fb38c143a9d1fa
nadia-pisarello <80127311+nadia-pisarello@users.noreply.github.com>
2021-08-24 19:38:14 -0300

    registrando los modelos de Pregunta y Respuesta
    De esta manera el admin podrá ingresar, modificar y eliminar preguntas y respuestas

--- src/apps/preguntas/admin.py
+++ src/apps/preguntas/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+#para poder utilizar los models de preguntas
+from preguntas.models import Pregunta, Respuesta
 
 # Register your models here.
+# para que el admin puedar gestionarlo
+admin.site.register(Pregunta)
+admin.site.register(Respuesta)
+

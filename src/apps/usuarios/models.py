74414fb31302621af9d7391e058ede01f9053af4
francokohn <86165038+francokohn@users.noreply.github.com>
2021-08-23 21:39:18 -0300

    agregue datos de usuarios

--- src/apps/usuarios/models.py
+++ src/apps/usuarios/models.py
@@ -6,6 +6,8 @@ class Usuario(AbstractUser):
 	nickname = models.CharField(max_length=100, null=True, blank=True)
 	localidad = models.CharField(max_length=255, null=True, blank=True)
 	puntos = models.IntegerField(null=True, blank=True)
+	es_admin = models.BooleanField(default=False)
+	ult_acceso = models.CharField(max_length=200, null=True, blank=True)
 
 	class Meta:
 		db_table = 'usuario'

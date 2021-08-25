89a24f1520a002c38ec2b116e8166057df7b3ec2
nadia-pisarello <80127311+nadia-pisarello@users.noreply.github.com>
2021-08-25 11:25:20 -0300

    Actualizado el __str__

--- src/apps/preguntas/models.py
+++ src/apps/preguntas/models.py
@@ -2,15 +2,13 @@ from django.db import models
 
 class Pregunta(models.Model):
     texto_pregunta = models.CharField(max_length=255)
-    respuestas = models.ForeignField('Respuesta', on_delete=models.CASCADE,)
-
-    #id_partida = models.ManyToManyField('Partida')
+  
 
     class Meta:
         db_table = 'pregunta'
 
     def __str__(self):
-        return self.nombre
+        return self.texto_pregunta
 
 
 class Respuesta(models.Model):
@@ -22,4 +20,4 @@ class Respuesta(models.Model):
         db_table = 'respueta'
 
     def __str__(self):
-        return self.nombre
+        return self.texto_respuesta

656cb4f1785a1aa408947affe9559b7425cb23e6
francokohn <86165038+francokohn@users.noreply.github.com>
2021-08-26 14:47:53 -0300

    elimino carpeta " app proyecto final"

--- src/apps/ProyectoFinal/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Usuario
-
-class UsuariosAdmin(admin.ModelAdmin):
-	list_display = ['id','nombre','nickname','localidad','ult_acceso','correo','es_admin']
-
-admin.site.register(Usuario, UsuariosAdmin)

8fdbb4dfb4c5de8ffb88d12269be6419b771812b
francokohn <86165038+francokohn@users.noreply.github.com>
2021-08-25 21:59:22 -0300

    comento funcionalidades

--- src/apps/preguntas/views.py
+++ src/apps/preguntas/views.py
@@ -1,11 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Pregunta
 from .forms import PreguntaForm
+from django.views.generic import CreateView
 
 def crear_pregunta(request):
 	template_name = "preguntas/newquestion.html"
-	#lista_de_preguntas = Pregunta.objects.all()
-	#print(lista_de_preguntas)
+
+	#if request.method == "POST":
+	#	texto_pregunta = request.POST.get("texto_pregunta",None)
+	#	categoria =  request.POST.get("categoria",None)
+	#	p = Pregunta.objects.create(texto_pregunta=texto_pregunta,categoria=categoria)
+	#	return redirect('PAGINA DE PREGUNTAS LISTADAS')
+
 	ctx = {
 		 'form': PreguntaForm()
 	}

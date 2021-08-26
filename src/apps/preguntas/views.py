4b6c537c20e7c0856065fcceb3a9330f3e605282
francokohn <86165038+francokohn@users.noreply.github.com>
2021-08-25 21:28:03 -0300

    edito vista crear pregunta

--- src/apps/preguntas/views.py
+++ src/apps/preguntas/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Pregunta
+from .forms import PreguntaForm
 
 def crear_pregunta(request):
 	template_name = "preguntas/newquestion.html"
-	ctx = { }
+	#lista_de_preguntas = Pregunta.objects.all()
+	#print(lista_de_preguntas)
+	ctx = {
+		 'form': PreguntaForm()
+	}
 
 	return render(request,template_name,ctx)

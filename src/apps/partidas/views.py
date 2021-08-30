27374314fdaefed51e79fad0c5210efbef4693d3
nadia-pisarello <pisarello.nads@gmail.com>
2021-08-29 23:14:44 -0300

    agregar vistas y lógicas para partidas

--- src/apps/partidas/views.py
+++ src/apps/partidas/views.py
@@ -1,13 +1,18 @@
-from django.shortcuts import render
-from django.contrib.auth.decorators import login_required, user_passes_test
+from django.shortcuts import redirect, render
+from django.contrib.auth.decorators import login_required
 from utils.user_test import es_admin
-from apps.preguntas.models import Partida
+from apps.preguntas.models import Partida, Pregunta, Respuesta
+import math
+import random
+
+RUTA_INICIO = 'usuarios:inicio'
+SESSION_KEY = 'resultado'
 
 @login_required()
 def listar(request):
     template_name = "partidas/listar.html"
     user = request.user
-    partidas = Partida.objects.all() if es_admin(user) else Partida.objects.filter(pk = user.id)
+    partidas = Partida.objects.all() if es_admin(user) else Partida.objects.filter(usuario_id = user.id)
     
     ctx = {
 		'partidas' : partidas
@@ -15,7 +20,64 @@ def listar(request):
 
     return render(request, template_name, ctx)
 
-
+@login_required()
 def jugar(request):
-    template_name = "partidas/jugar.html"
-    return render(request, template_name, {})
+    user = request.user
+
+    if request.method == 'POST':
+        resultados, puntaje = get_resultado(request.POST)
+        user.partida_set.create(puntos = puntaje)
+
+        request.session[SESSION_KEY] = { 
+            'resultados': resultados,
+            'puntaje': puntaje
+        }
+
+        return redirect('partidas:resultado')
+    else:
+        template_name = "partidas/jugar.html"
+        nivel = get_nivel(request.user)
+        preguntas = get_preguntas(nivel)
+        ctx = { 'preguntas': preguntas }
+
+        return render(request, template_name, ctx)
+
+def resultado(request):
+    template_name = "partidas/resultado.html"
+
+    if SESSION_KEY in request.session:
+        ctx = request.session[SESSION_KEY]
+        del request.session[SESSION_KEY]
+        return render(request, template_name, ctx)
+    else:
+        redirect(RUTA_INICIO)
+
+def get_nivel(jugador):
+    try:
+        puntaje_maximo = jugador.partida_set.latest('puntos')
+        puntos = puntaje_maximo.puntos
+        return math.floor(puntos / 10)
+    except Partida.DoesNotExist:
+        return 0
+
+def get_preguntas(nivel):    
+    ids = [i.id for i in Pregunta.objects.filter(nivel = nivel)]
+    random.shuffle(ids)
+    return [Pregunta.objects.get(id=i) for i in ids[:10]]
+
+def get_resultado(request):
+    puntaje = 0
+    resultados = list()
+    for key, value in request.items():
+        if 'pta-' in key:
+            rta = Respuesta.objects.get(pk = value)
+            id_pta = key.split('-')[1]
+            pta = Pregunta.objects.get(pk = id_pta)
+
+            if (rta.es_correcta):
+                resultados.append((pta.texto_pregunta, rta.texto_respuesta, True))
+                puntaje += pta.nivel + 1
+            else:
+                resultados.append((pta.texto_pregunta, rta.texto_respuesta, False))
+
+    return (resultados, puntaje)
--- src/apps/preguntas/models.py
+++ src/apps/preguntas/models.py
@@ -1,18 +1,34 @@
 from django.db import models
 from django.db.models.deletion import CASCADE
 from apps.usuarios.models import Usuario
+import random
 
 class Pregunta(models.Model):
     texto_pregunta = models.CharField(max_length=255)
-    respuesta_correcta = models.CharField(max_length=255)
-    respuesta_incorrecta_1 = models.CharField(max_length=255)
-    respuesta_incorrecta_2 = models.CharField(max_length=255)
+    nivel = models.IntegerField()
 
     class Meta:
         db_table = 'Pregunta'
 
     def __str__(self):
         return self.texto_pregunta
+
+    def get_respuestas(self):
+        # traer todas las respuestas relacionadas a esta pregunta
+        respuestas = self.respuesta_set
+        # filtrar las incorrectas, crear una lista con sus ids y randomizarlos
+        ids = [i.id for i in respuestas.filter(es_correcta = False)]
+        random.shuffle(ids)
+        # tomar muestra de 2 incorrectas
+        subset = ids[:2]
+        # buscamos la correcta
+        correcta = respuestas.get(es_correcta = True)
+        # agregamos el id de la correcta a la lista de incorrectas
+        subset.append(correcta.id)
+        # mezclamos de nuevo
+        random.shuffle(subset)
+        # devolvemos los objetos de los ids como nos quedaron
+        return [respuestas.get(pk=i) for i in subset]
 
 class Partida(models.Model):
     usuario = models.ForeignKey(Usuario, on_delete = models.CASCADE)
@@ -21,3 +37,14 @@ class Partida(models.Model):
 
     class Meta:
         db_table = 'Partida'
+
+class Respuesta(models.Model):
+    texto_respuesta = models.CharField(max_length=255)
+    pregunta = models.ForeignKey(Pregunta, on_delete = models.CASCADE)
+    es_correcta = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = 'Respuesta'
+
+    def __str__(self):
+        return self.texto_respuesta
